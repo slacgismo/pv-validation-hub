fd42ca788686d8347518dd80ba42c3804a99e700
Mengdi Zhu <mengdiz@andrew.cmu.edu>
2022-10-13 14:31:47 -0400

    get secrets

--- aws/scripts/after_install.sh
+++ aws/scripts/after_install.sh
@@ -17,6 +17,11 @@ python3 -m pip install -r requirements.txt
 # python3 manage.py makemigrations backend
 # python3 manage.py migrate
 
+# Retrieve secrets
+aws s3 cp s3://validationhubpipelinedjang-webappdeploymentbucket-1jov8z4e3glao/secrets ~/pv-validation-hub/secrets
+cat secrets >> ~/.bashrc
+source ~/.bashrc
+
 # Start
-cd valhub
+cd ~/pv-validation-hub/valhub
 python3 manage.py runserver
