213b06a3854aa9dda302a69553fcd11a28c4bf00
aanandak97 <aanandak97@andrew.cmu.edu>
2022-10-14 16:01:18 -0400

    Base Commit
    Base Frontend Commit

---
+++ frontend/pv-validation-hub/components/Login/login.js
@@ -0,0 +1,24 @@
+import { Fragment } from 'react';
+import React, {Container} from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { connect } from 'react-redux';
+import { Component } from 'react/cjs/react.production.min';
+import { Form, Email, Password, Button } from 'redux-valid-form';
+import PropTypes from 'prop-types';
+
+class Login extends Component{
+    
+    /*add Validation*/
+    
+
+    render(){
+        return(
+            <Fragment>
+                <Form>
+                    
+                </Form>
+            </Fragment>
+        )
+    }
+}
---
+++ frontend/pv-validation-hub/services/dashboard-api.js
@@ -0,0 +1,3 @@
+// const generateAnalysisData() = (analysisId) => {
+
+// }
