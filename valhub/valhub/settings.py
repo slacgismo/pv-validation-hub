6cc03a88ed6a636bf8e6e4d28ba25fcb96fbf456
Mengdi Zhu <mengdiz@andrew.cmu.edu>
2022-10-27 00:18:45 -0400

    bug fix

--- valhub/valhub/settings.py
+++ valhub/valhub/settings.py
@@ -76,7 +76,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = get_secret("DjangoSecretKey")['DJANGO_SECRET_KEY']
+try:
+    SECRET_KEY = get_secret("DjangoSecretKey")['DJANGO_SECRET_KEY']
+except:
+    SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
