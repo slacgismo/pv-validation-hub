eece546a0fd9f50d5fc9b6a544b6c055cc67c7ff
yuweiliandrew <yuweili@andrew.cmu.edu>
2022-10-30 23:22:15 -0700

    add account models

--- valhub/accounts/models.py
+++ valhub/accounts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Account(models.Model):
+    """
+    Model to store a user account
+    """
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    githubLink = models.URLField(max_length=200, null=True, blank=True)
+    def __str__(self) -> str:
+        return "{}".format(self.user)
+    class Meta:
+        app_label = "accounts"
+        db_table = "user_account"
