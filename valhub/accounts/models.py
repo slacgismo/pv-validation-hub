3daf150b5fb0c8ead2ac10d494ad04d1d1f8bbdf
yuweiliandrew <yuweili@andrew.cmu.edu>
2022-11-24 13:23:16 -0800

    change api to rest framework

--- valhub/accounts/models.py
+++ valhub/accounts/models.py
@@ -7,10 +7,15 @@ class Account(models.Model):
     """
     Model to store a user account
     """
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    # user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user_id = models.IntegerField(primary_key=True)
+    username = models.CharField(max_length=32)
+    password = models.CharField(max_length=32)
+    email = models.CharField(max_length=100, null=True)
     githubLink = models.URLField(max_length=200, null=True, blank=True)
     def __str__(self) -> str:
-        return "{}".format(self.user)
+        return "{}".format(self.username)
+        # return "{}".format(self.user.get_username())
     class Meta:
         app_label = "accounts"
         db_table = "user_account"
--- valhub/valhub/settings.py
+++ valhub/valhub/settings.py
@@ -95,6 +95,7 @@ ALLOWED_HOSTS = ['*', ]
 INSTALLED_APPS = [
     'backend.apps.BackendConfig',
     'accounts.apps.AccountsConfig',
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
