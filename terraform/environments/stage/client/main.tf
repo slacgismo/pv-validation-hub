b7233fca8e378d4fa3c4264068e56489146a6d52
thistleman <codes5096@gmail.com>
2023-01-19 15:06:31 -0800

    Updated to add terraform structure, dockerfiles

---
+++ terraform/environments/stage/client/main.tf
@@ -0,0 +1,15 @@
+resource "aws_instance" "default" {
+    ami                    = var.ami-id
+    iam_instance_profile   = var.iam-instance-profile
+    instance_type          = var.instance-type
+    key_name               = var.key-pair
+    private_ip             = var.private-ip
+    subnet_id              = var.subnet-id
+    vpc_security_group_ids = var.vpc-security-group-ids
+}
+
+tags = {
+    project    = "pv-insight"
+    Name       = "pv-validation-hub"
+    codeDeploy = "pv-validation-hub"
+}
