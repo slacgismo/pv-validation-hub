0ac91ebe626c51692d3a4fc3b1884daaaa749fa5
thistleman <codes5096@gmail.com>
2023-03-22 16:06:07 -0700

    remove sudo

--- node-docker-ecs/docker-entrypoint.sh
+++ node-docker-ecs/docker-entrypoint.sh
@@ -1,5 +1,5 @@
 #/bin/sh
 
 cd /app
-sudo iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 3000
+iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 3000
 node index.js
