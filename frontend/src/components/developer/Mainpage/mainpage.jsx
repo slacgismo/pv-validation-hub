ac6fc42b20fff9c928d488a80b9c8e9672da9e23
Willendless <317500141@qq.com>
2023-03-28 18:04:25 -0400

    feat: implement developer private report page

--- frontend/src/components/developer/Mainpage/mainpage.jsx
+++ frontend/src/components/developer/Mainpage/mainpage.jsx
@@ -3,8 +3,6 @@ import { Container } from "@mui/system";
 import Box from '@mui/material/Box';
 import Divider from '@mui/material/Divider';
 import Drawer from '@mui/material/Drawer';
-import IconButton from '@mui/material/IconButton';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
 import AssessmentIcon from '@mui/icons-material/Assessment';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
@@ -12,14 +10,16 @@ import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import Checkbox from '@mui/material/Checkbox';
-import MailIcon from '@mui/icons-material/Mail';
 import ArticleIcon from '@mui/icons-material/Article';
-import CommentIcon from '@mui/icons-material/Comment'
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
 import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
+import BookmarkIcon from '@mui/icons-material/Bookmark';
 import Grid from '@mui/material/Grid';
-
-const drawerWidth = 240;
+import SubmissionReport from '../Report/report';
+import SummarizeIcon from '@mui/icons-material/Summarize';
+import QueryBuilderIcon from '@mui/icons-material/QueryBuilder';
+import DoneIcon from '@mui/icons-material/Done';
+import { useState } from "react";
 
 export default function DeveloperHome() {
   const [mobileOpen, setMobileOpen] = React.useState(false);
@@ -28,88 +28,121 @@ export default function DeveloperHome() {
     setMobileOpen(!mobileOpen);
   };
 
+  const [showComponent, setShowComponent] = useState('home');
+
   // const container = window !== undefined ? () => window().document.body : undefined;
 
+  const handleNavClick = (component) => {
+    setShowComponent(component);
+  }
+
+  const renderComponent = () => {
+    if (showComponent === 'home') {
+      return <Home onClick={() => handleNavClick('report')}/>;
+    } else if (showComponent === 'report') {
+      return <SubmissionReport />;
+    }
+  }
+
+  const navs = [
+    {
+      title: 'Submissions',
+      icon: <AccessTimeIcon />,
+      handler: () => { handleNavClick("home") },
+    },
+    {
+      title: 'Participanted Analyses',
+      icon: <AssessmentIcon />,
+      handler: () => {},
+    },
+    {
+      title: 'Used Datasets',
+      icon: <ArticleIcon />,
+      handler: () => {},
+    }
+  ];
+
   return (
-    <Container sx={{ position: 'fixed', top: '10%' }}>
-          <Grid container alignItems="center" spacing={2}>
-            <Grid item xs = {3}>
-              <Box component="Nav" aria-label="mailbox folders" sx={{display: 'flex'}}>
-                <Drawer variant="permanent"  sx={{ top: '10%', border: '1px solid rgba(0, 0, 0, 0.12)', }} open>
-                  <Toolbar />
-                  <Divider />
-                  <List>
-                    {['Submissions', 'Participated Analyses', 'Used Datasets'].map((text, index) => (
-                      <ListItem
-                        key={text}
-                        disablePadding>
-                        <ListItemButton>
-                          <ListItemIcon>
-                            {index % 2 === 0 ? <ArticleIcon /> : <AssessmentIcon />}
-                          </ListItemIcon>
-                          <ListItemText primary={text} />
-                        </ListItemButton>
-                      </ListItem>
-                    ))}
-                  </List>
-                  <Divider />
-                  <List>
-                    {['All mail'].map((text, index) => (
-                      <ListItem key={text} disablePadding>
-                        <ListItemButton>
-                          <ListItemIcon>
-                            {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                          </ListItemIcon>
-                          <ListItemText primary={text} />
-                        </ListItemButton>
-                      </ListItem>
-                    ))}
-                  </List>
-                </Drawer>
-              </Box>
-            </Grid>
+    <Container sx={{position: 'absolute'}}>
+      <Grid container alignItems="center" spacing={2}>
+        <Grid item xs = {3}>
+          <Box component="Nav" aria-label="mailbox folders" sx={{ position: 'fixed', display: 'flex'}}>
+            <Drawer variant="permanent"  sx={{ top: '10%', border: '1px solid rgba(0, 0, 0, 0.12)', }} open>
+              <Toolbar />
+              <Divider />
+              <List>
+                {navs.map((item) => (
+                  <ListItem key={item.title} disablePadding>
+                    <ListItemButton onClick={item.handler}>
+                      <ListItemIcon>
+                        {item.icon}
+                      </ListItemIcon>
+                      <ListItemText primary={item.title} />
+                    </ListItemButton>
+                  </ListItem>
+                ))}
+              </List>
+              <Divider />
+              <List>
+                {['Bookmark'].map((text, index) => (
+                  <ListItem key={text} disablePadding>
+                    <ListItemButton>
+                      <ListItemIcon>
+                        <BookmarkIcon />
+                      </ListItemIcon>
+                      <ListItemText primary={text} />
+                    </ListItemButton>
+                  </ListItem>
+                ))}
+              </List>
+            </Drawer>
+          </Box>
+        </Grid>
 
-            <Grid item xs = {9}>
-                <Box
-                  component="main"
-                  display="flex"
-                  justifyContent="center"
-                  alignItems="center"
-                  sx={{
-                    overflow: 'auto',
-                    border: '1px solid rgba(0, 0, 0, 0.12)',
-                    }}
-                >
-                  <Toolbar />
-                    <List sx={{ width: '100%', bgcolor: 'background.paper' }}>
-                      {[0, 1, 2, 3].map((value) => {
-                        const labelId = `checkbox-list-label-${value}`;
-                        return (
-                          <ListItem
-                            key={value}
-                            disablePadding
-                          >
-                            <ListItemButton role={undefined} dense>
-                              <ListItemText id={labelId} primary={`Submission record ${value + 1}`} />
-                              <ListItemIcon>
-                                <CommentIcon />
-                                <Checkbox
-                                  edge="start"
-                                  // checked={checked.indexOf(value) !== -1}
-                                  tabIndex={-1}
-                                  disableRipple
-                                  inputProps={{ 'aria-labelledby': labelId }}
-                                />
-                              </ListItemIcon>
-                            </ListItemButton>
-                          </ListItem>
-                        );
-                      })}
-                  </List>
-                </Box>
-            </Grid>
-          </Grid>
-
+        <Grid item xs = {9} style={{ marginTop: '2%' }}>
+            <Box
+              component="main"
+              display="flex"
+              // justifyContent="center"
+              // alignItems="center"
+            >
+              <Toolbar />
+              {renderComponent()}
+            </Box>
+        </Grid>
+      </Grid>
     </Container>
   );
+}
+
+function Home({ onClick }) {
+  return (
+    <List sx={{ width: '100%', bgcolor: 'background.paper' }}>
+      {[1, 2, 3, 4].map((value, index) => {
+        const labelId = `checkbox-list-label-${value}`;
+        return (
+          <ListItem key={value} disablePadding>
+            <ListItemButton dense>
+              <ListItemIcon>
+                <Checkbox
+                  edge="start"
+                  // checked={checked.indexOf(value) !== -1}
+                  tabIndex={-1}
+                  disableRipple
+                  inputProps={{ 'aria-labelledby': labelId }}
+                />
+              </ListItemIcon>
+              <ListItemIcon>
+                { index % 2 === 0? <DoneIcon /> : <QueryBuilderIcon/>}
+              </ListItemIcon>
+              <ListItemText id={labelId} primary={`Submission ${value}`} />
+              <ListItemIcon onClick={onClick}>
+                <SummarizeIcon />
+              </ListItemIcon>
+            </ListItemButton>
+          </ListItem>
+        );
+      })}
+    </List>
+  )
 }
