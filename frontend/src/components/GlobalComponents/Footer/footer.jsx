bdf0e8fc0bb5930b23af0f42c8c4ba1e73735da7
Willendless <317500141@qq.com>
2023-04-10 16:35:55 -0400

    refactor: improve footer

--- frontend/src/components/GlobalComponents/Footer/footer.jsx
+++ frontend/src/components/GlobalComponents/Footer/footer.jsx
@@ -1,6 +1,5 @@
 import { Grid, Link, Typography } from "@mui/material";
 import { Box } from "@mui/system";
-import DiamondIcon from '@mui/icons-material/Diamond';
 
 import {
     FacebookIcon,
@@ -12,69 +11,67 @@ import {
     WhatsappIcon,
     WhatsappShareButton,
 } from "react-share";
+
 const Footer = () => {
     const shareUrl = "http://localhost:3000";
     const title = "PV Validation Hub"
     return (
         <Box sx={{
-            height: 200,
-            minHeight: 200,
-            maxHeight: 300,
+            height: 150,
+            minHeight: 100,
+            maxHeight: 200,
         }}>
-            <Grid container spacing={2}>
+            <Grid container spacing={2} alignItems="center" justify="center">
                 <Grid item xs={12}>
-                    <Box sx={{ borderBottom: 1, borderColor: 'divider', marginTop: 4 }}>
-                        <Box sx={{ marginLeft: 9 }}>
-                            <Grid container spacing={2}>
-                                <Grid item xs={2}>
-                                    <Link href="https://www6.slac.stanford.edu/news-and-events/connect-with-us" underline="hover" color="inherit">
-                                        Community
-                                    </Link>
-                                </Grid>
-                                <Grid item xs={2}>
-                                    <Link href="https://www6.slac.stanford.edu/about" underline="hover" color="inherit">
-                                        Company
-                                    </Link>
-
-                                </Grid>
-                                <Grid item xs={4}>
-                                    <Box sx={{ justifyContent: 'center', marginLeft: 17 }}>
-                                        <DiamondIcon sx={{ display: { xs: 'none', md: 'flex', color: "black", marginLeft: 15 }, mr: 1 }} />
-                                        <Typography
-                                            variant="h6"
-                                            noWrap
-                                            component="a"
-                                            href="/"
-                                            sx={{
-                                                mr: 2,
-                                                display: { xs: 'none', md: 'flex' },
-                                                fontFamily: 'monospace',
-                                                fontWeight: 700,
-                                                letterSpacing: '.3rem',
-                                                color: 'black',
-                                                textDecoration: 'none',
-                                            }}
-                                        >
-                                            LOGO
-                                        </Typography>
-                                    </Box>
-                                </Grid>
-                                <Grid item xs={2}>
-                                    <Link href="https://www6.slac.stanford.edu/news-and-events/news-center" underline="hover" color="inherit">
-                                        Blog
-                                    </Link>
-                                </Grid>
-                                <Grid item xs={2}>
-                                    <Link href="https://www6.slac.stanford.edu/about/resources" underline="hover" color="inherit">
-                                        Resources
-                                    </Link>
-                                </Grid>
+                    <Box display="flex" justifyContent="center" alignItems="center" sx={{ borderBottom: 1, borderColor: 'divider', marginTop: 4 }}>
+                        <Grid item xs={2}></Grid>
+                        <Grid container spacing={2} xs={8}>
+                            <Grid item xs={2}>
+                                <Link href="https://www6.slac.stanford.edu/news-and-events/connect-with-us" underline="hover" color="inherit">
+                                    Community
+                                </Link>
                             </Grid>
-                        </Box>
+                            <Grid item xs={2}>
+                                <Link href="https://www6.slac.stanford.edu/about" underline="hover" color="inherit">
+                                    Company
+                                </Link>
+                            </Grid>
+                            <Grid item xs={4}>
+                                <Box alignItems="center" justifyContent='center'>
+                                    <Typography
+                                        display="block"
+                                        align="center"
+                                        variant="h6"
+                                        noWrap
+                                        Href="/"
+                                        sx={{
+                                            mr: 2,
+                                            fontFamily: 'sans-serif',
+                                            fontWeight: 700,
+                                            color: 'black',
+                                            textDecoration: 'none',
+                                        }}
+                                    >
+                                        PVHub
+                                    </Typography>
+                                </Box>
+                            </Grid>
+                            <Grid item xs={2}>
+                                <Link href="https://www6.slac.stanford.edu/news-and-events/news-center" underline="hover" color="inherit">
+                                    Blog
+                                </Link>
+                            </Grid>
+                            <Grid item xs={2}>
+                                <Link href="https://www6.slac.stanford.edu/about/resources" underline="hover" color="inherit">
+                                    Resources
+                                </Link>
+                            </Grid>
+                        </Grid>
+                        <Grid item xs={2}></Grid>
                     </Box>
                 </Grid>
                 <Grid item xs={12}>
-                    <Box sx={{ marginTop: 1, marginLeft: 72 }}>
+                    <Box display="flex" justifyContent="center" alignItems="center">
                         <FacebookShareButton
                             url={shareUrl}
                             quote={title}
