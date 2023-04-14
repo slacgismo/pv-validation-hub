d756906d0fd62a1d9a827ced21a8630f6735751d
thistleman <codes5096@gmail.com>
2023-04-14 13:06:48 -0700

    mods

--- valhub/valhub/settings.py
+++ valhub/valhub/settings.py
@@ -227,8 +227,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
+if hostname is not None:
+    STATIC_URL = 's3://pv-validation-hub-bucket/static/'
+else:
+    STATIC_URL = 's3:5000/pv-validation-hub-bucket/static/'
 
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
