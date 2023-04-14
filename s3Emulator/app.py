f27ef65dc0011abfbdb3b7144b93a447bc376d02
thistleman <codes5096@gmail.com>
2023-04-14 11:29:40 -0700

    updated s3 emulation

--- s3Emulator/app.py
+++ s3Emulator/app.py
@@ -1,21 +1,26 @@
 from flask import Flask, request
-from boto3.session import Session
+import os
 
 app = Flask(__name__)
 
 # Define a simple S3-like API
-@app.route("/<bucket_name>/<object_name>", methods=["PUT"])
+@app.route("/<bucket_name>/<path:object_name>", methods=["PUT"])
 def put_object(bucket_name, object_name):
     # Get the content of the object from the request
     object_content = request.data
 
-    # Store the object in a local file system
-    with open(f"{bucket_name}/{object_name}", "wb") as f:
+    # Create the local directory for the bucket if it doesn't exist
+    bucket_dir = f"{bucket_name}/"
+    if not os.path.exists(bucket_dir):
+        os.makedirs(bucket_dir)
+
+    # Store the object in the local file system
+    with open(f"{bucket_dir}{object_name}", "wb") as f:
         f.write(object_content)
 
     return "", 204
 
-@app.route("/<bucket_name>/<object_name>", methods=["GET"])
+@app.route("/<bucket_name>/<path:object_name>", methods=["GET"])
 def get_object(bucket_name, object_name):
     # Retrieve the object from the local file system
     try:
@@ -27,4 +32,4 @@ def get_object(bucket_name, object_name):
     return object_content, 200
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=5000)
