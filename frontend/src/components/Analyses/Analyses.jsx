4a4b2468f7cd705c339b99953afeb6f6d37918f2
Duncan Ragsdale <88173870+Thistleman@users.noreply.github.com>
2023-10-06 16:40:01 -0700

    fixing ci/cd, adding wrapper for admin ec2 container

--- frontend/src/components/Analyses/Analyses.jsx
+++ frontend/src/components/Analyses/Analyses.jsx
@@ -42,7 +42,24 @@ export default function Dashboard() {
                 <Grid item>
                     <Box sx={{ flexGrow: 1, marginTop: 3 }}>
                         <Typography variant="body1" gutterBottom>
-                            The Validation Hub is intended to be a clearinghouse for the transfer of novel algorithms and software from the research community to industry. The primary function of the Hub will be for developers to submit executable code which will run on hosted data sets. Developers will receive private reports on the accuracy and performance (e.g., run-time) of the submitted algorithms, and public high level summaries will be hosted. These summaries will indicate the organization who submitted the algorithm (e.g., links to GitHub pages, documentation websites, etc.), high-level accuracy metrics, and standardized performance metrics. These results will be stored in a publicly available database, accessible through the Hub, with the ability for users to sort and filter the results. In short, the Hub will be presented to public users as a collection of interactive leaderboards, organized around specific analysis tasks pertinent to the PV data science community. These tasks include things such as the estimation of various PV loss factors and the detection of various operational issues, to be defined in this document.
+                            The Validation Hub is intended to be a clearinghouse 
+                            for the transfer of novel algorithms and software from the 
+                            research community to industry. The primary function of the 
+                            Hub will be for developers to submit executable code which will 
+                            run on hosted data sets. Developers will receive private reports 
+                            on the accuracy and performance (e.g., run-time) of the submitted 
+                            algorithms, and public high level summaries will be hosted. These 
+                            summaries will indicate the organization who submitted the algorithm 
+                            (e.g., links to GitHub pages, documentation websites, etc.), 
+                            high-level accuracy metrics, and standardized performance metrics. 
+                            These results will be stored in a publicly available database, 
+                            accessible through the Hub, with the ability for users to sort and 
+                            filter the results. In short, the Hub will be presented to public 
+                            users as a collection of interactive leaderboards, organized around 
+                            specific analysis tasks pertinent to the PV data science community. 
+                            These tasks include things such as the estimation of various PV loss 
+                            factors and the detection of various operational issues, to be 
+                            defined in this document.
                         </Typography>
                     </Box>
                 </Grid>
