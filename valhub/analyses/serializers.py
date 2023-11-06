fe50906dd90354ac05c21f1979c862af95f4e2ce
Duncan Ragsdale <88173870+Thistleman@users.noreply.github.com>
2023-11-06 10:40:29 -0800

    fixed bugs in models/serializers. Added migrations for db drift/model tracking.

--- valhub/analyses/serializers.py
+++ valhub/analyses/serializers.py
@@ -7,15 +7,11 @@ class AnalysisSerializer(serializers.ModelSerializer):
     Serialize the Analysis Model.
     """
 
-    def __init__(self, *args, **kwargs):
-        super(AnalysisSerializer, self).__init__(*args, **kwargs)
-
     class Meta:
         model = Analysis
-        fields = ("analysis_name")
+        fields = ("analysis_id", "analysis_name")
 
     def to_representation(self, instance):
-        data = super(AnalysisSerializer, self).to_representation(instance)
+        data = super().to_representation(instance)
         data["analysis_id"] = instance.analysis_id
-        data["analysis_name"] = instance.analysis_name
         return data
