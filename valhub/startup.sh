7e0928b7aea2f759a7917046e82af76fc3324962
Mitchell Victoriano <mitchell.victoriano@gmail.com>
2024-12-05 17:17:55 -0800

    supervisord incorrectly assumed to have dependency feature. converted entrypoint to bash script

---
+++ valhub/startup.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -e  # Exit immediately if a command exits with a non-zero status
+
+# Define the directory
+DIR="/root/valhub"
+
+cd $DIR
+
+# Run valhub_makemigrations
+echo "Running makemigrations..."
+python3 manage.py makemigrations
+
+# Run valhub_migrate
+echo "Running migrations..."
+python3 manage.py migrate
+
+# Run valhub_initial_data
+echo "Loading initial data..."
+python3 manage.py loaddata initial_data.json
+
+# Run valhub_collectstatic
+echo "Collecting static files..."
+python3 manage.py collectstatic --noinput
+
+# Run valhub_superuser
+echo "Creating superuser..."
+{
+    python3 manage.py createsuperuser --noinput
+} || {
+    echo "Superuser already exists, skipping creation."
+}
+
+# Run valhub_webserver
+echo "Starting webserver..."
+python3 manage.py runserver 0.0.0.0:8005
