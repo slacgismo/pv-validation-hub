e1bb84a4670ee6248f27edd120ae0372a86c2ab7
Mitchell Victoriano <mitchell.victoriano@gmail.com>
2025-03-13 11:03:03 -0700

    Added entry point for aggregation script

---
+++ ec2/aggregate.py
@@ -0,0 +1,10 @@
+def main(analysis_id: int, output_file_path: str):
+    print("Hello from aggregate.py")
+
+
+if __name__ == "__main__":
+    # Inputs
+    analysis_id = 1
+    output_file_path = "output.csv"
+
+    main(analysis_id, output_file_path)
